6dbd9ec50cc9beacacb9ee28ea85a4df506f3f6b
Natanael Lima <taelima1997@gmail.com>
2022-03-15 23:57:50 -0300

    add CreateUserUseCase

---
+++ src/modules/client/useCases/createClient/createClientUseCase.ts
@@ -0,0 +1,33 @@
+import { hash } from "bcrypt";
+import prismaClient from "../../../../database";
+
+interface ICreateUser {
+  username: string;
+  password: string;
+}
+
+export class CreateUserUseCase {
+  async execute({ password, username }: ICreateUser) {
+    const clientExists = await prismaClient.client.findFirst({
+      where: {
+        username: {
+          mode: "insensitive",
+          equals : username
+        },
+      },
+    });
+
+    console.log({clientExists})
+    if (clientExists) {
+      throw new Error("Client already exists");
+    }
+
+    const hashPassword = await hash(password, 10);
+
+    const client = await prismaClient.client.create({
+      data: { password: hashPassword, username },
+    });
+
+    return client;
+  }
+}
