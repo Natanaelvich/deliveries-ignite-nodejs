53fc9b80442c52183e5eb8a0064f685ff52a6b9f
Natanael Lima <taelima1997@gmail.com>
2022-03-15 23:57:43 -0300

    update with handler error, routes and express use json

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
-import express from 'express'
-import './database'
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import routes from "./routes";
 
-const app = express()
+const app = express();
 
-app.listen(3000)
+app.use(express.json());
+
+app.use(routes);
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(err.status ?? 500);
+  res.json({
+    success: false,
+    error: err.message || "Internal error occured",
+  });
+
+  next(err);
+});
+
+app.listen(3000);
