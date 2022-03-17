ddbca8fc0aec997c31f973512675996d47ea20ee
Natanael Lima <taelima1997@gmail.com>
2022-03-16 23:58:53 -0300

    add "/client/signin" route

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
+import { AuthenticateClientController } from "./modules/account/useCases/authenticateClient/AuthenticateClientController";
 import { CreateClientController } from "./modules/client/useCases/createClient/CreateClientController";
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
+const authenticateClientController = new AuthenticateClientController()
 
 routes.post("/client", createClientController.handle);
+routes.post("/client/signin", authenticateClientController.handle);
 
 
 export default routes
