b8103fac84057686e6edd86fed89ef75c29a21fd
Natanael Lima <taelima1997@gmail.com>
2022-03-18 23:58:28 -0300

    add RefreshClientTokenController

---
+++ src/modules/account/useCases/refreshClientToken/RefreshClientTokenController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { RefreshClientTokenUseCase } from "./RefreshClientTokenUseCase";
+
+export class RefreshClientTokenController {
+  async handle(request: Request, response: Response) {
+    const { refreshToken } = request.body;
+
+    const { sub } = verify(refreshToken, process.env.JWT_SECRET_REFRESH || "secret");
+
+    if (typeof sub === "string") {
+      const refreshClientTokenUseCase = new RefreshClientTokenUseCase();
+
+      const refreshClientToken = await refreshClientTokenUseCase.execute({
+        id_client: sub,
+        token: refreshToken,
+      });
+
+      response.json(refreshClientToken);
+    } else {
+      response.json({ message: "Invalid Token" }).status(401);
+    }
+  }
+}
