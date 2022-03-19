c932e38709c434679084760c5067b86d387137df
Natanaelvich <taelima1997@gmail.com>
2022-03-19 20:01:57 -0300

    add delivery create post

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateClientController } from "./modules/account/useCases/authenticateClient/AuthenticateClientController";
 import { RefreshClientTokenController } from "./modules/account/useCases/refreshClientToken/RefreshClientTokenController";
 import { CreateClientController } from "./modules/client/useCases/createClient/CreateClientController";
+import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router();
@@ -12,10 +13,14 @@ const refreshClientTokenController = new RefreshClientTokenController();
 
 const createDeliverymanController = new CreateDeliverymanController();
 
+const createDeliveryController = new CreateDeliveryController();
+
 routes.post("/client", createClientController.handle);
 routes.post("/client/signin", authenticateClientController.handle);
 routes.post("/client/refreshtoken", refreshClientTokenController.handle);
 
 routes.post("/deliveryman", createDeliverymanController.handle);
+
+routes.post("/delivery", createDeliveryController.handle);
 
 export default routes;
