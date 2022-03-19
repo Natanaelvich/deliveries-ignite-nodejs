ad8bfeff7b13f3933bc32646a435e9f282f9a914
Natanael Lima <taelima1997@gmail.com>
2022-03-18 23:58:50 -0300

    add route client/refreshtoken

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { AuthenticateClientController } from "./modules/account/useCases/authenticateClient/AuthenticateClientController";
+import { RefreshClientTokenController } from "./modules/account/useCases/refreshClientToken/RefreshClientTokenController";
 import { CreateClientController } from "./modules/client/useCases/createClient/CreateClientController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
@@ -7,11 +8,13 @@ const routes = Router();
 
 const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
+const refreshClientTokenController = new RefreshClientTokenController();
 
 const createDeliverymanController = new CreateDeliverymanController();
 
 routes.post("/client", createClientController.handle);
 routes.post("/client/signin", authenticateClientController.handle);
+routes.post("/client/refreshtoken", refreshClientTokenController.handle);
 
 routes.post("/deliveryman", createDeliverymanController.handle);
 
