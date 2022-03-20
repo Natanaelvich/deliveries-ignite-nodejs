496ad1b0749a6cbe9c317577c58da0530ef0921b
Natanael Lima <taelima1997@gmail.com>
2022-03-19 23:13:51 -0300

    add routes /deliveryman/deliveries/:idDeliveryMan /deliveryman/signin /deliveryman/refreshtoken

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { AuthenticateClientController } from "./modules/account/useCases/authenticateClient/AuthenticateClientController";
+import { AuthenticateDeliverymanController } from "./modules/account/useCases/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { RefreshClientTokenController } from "./modules/account/useCases/refreshClientToken/RefreshClientTokenController";
+import { RefreshDeliverymanTokenController } from "./modules/account/useCases/refreshDeliverymanToken/RefreshDeliverymanTokenController";
 import { CreateClientController } from "./modules/client/useCases/createClient/CreateClientController";
 import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
+import { GetDeliverisDeliverymanController } from "./modules/deliveryman/useCases/getDeliverisDeliveryman/GetDeliverisDeliverymanController";
 
 const routes = Router();
 
@@ -11,7 +14,11 @@ const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
 const refreshClientTokenController = new RefreshClientTokenController();
 
+
 const createDeliverymanController = new CreateDeliverymanController();
+const getDeliverisDeliverymanController = new GetDeliverisDeliverymanController();
+const authenticateDeliverymanController = new AuthenticateDeliverymanController();
+const refreshDeliverymanTokenController = new RefreshDeliverymanTokenController();
 
 const createDeliveryController = new CreateDeliveryController();
 
@@ -20,6 +27,9 @@ routes.post("/client/signin", authenticateClientController.handle);
 routes.post("/client/refreshtoken", refreshClientTokenController.handle);
 
 routes.post("/deliveryman", createDeliverymanController.handle);
+routes.get("/deliveryman/deliveries/:idDeliveryMan", getDeliverisDeliverymanController.handle);
+routes.post("/deliveryman/signin", authenticateDeliverymanController.handle);
+routes.post("/deliveryman/refreshtoken", refreshDeliverymanTokenController.handle);
 
 routes.post("/delivery", createDeliveryController.handle);
 
