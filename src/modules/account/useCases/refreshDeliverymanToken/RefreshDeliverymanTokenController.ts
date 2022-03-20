592c9e5bb949bd33f52a55500a6b320ea0331e18
Natanael Lima <taelima1997@gmail.com>
2022-03-19 23:13:06 -0300

    add RefreshDeliverymanTokenController

---
+++ src/modules/account/useCases/refreshDeliverymanToken/RefreshDeliverymanTokenController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { RefreshDeliverymanUseCase } from "./RefreshDeliverymanTokenUseCase";
+
+export class RefreshDeliverymanTokenController {
+  async handle(request: Request, response: Response) {
+    const { refreshToken } = request.body;
+
+    const { sub } = verify(refreshToken, process.env.JWT_SECRET_REFRESH || "secret");
+
+    if (typeof sub === "string") {
+      const refreshDeliverymanUseCase = new RefreshDeliverymanUseCase();
+
+      const refreshDeliverymanToken = await refreshDeliverymanUseCase.execute({
+        id_deliveryman: sub,
+        token: refreshToken,
+      });
+
+      response.json(refreshDeliverymanToken);
+    } else {
+      response.json({ message: "Invalid Token" }).status(401);
+    }
+  }
+}
