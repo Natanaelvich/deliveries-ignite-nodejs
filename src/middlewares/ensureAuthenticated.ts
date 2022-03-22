90cae279f5ea17432e43d44830ff6f985ca14013
Natanael Lima <taelima1997@gmail.com>
2022-03-21 23:58:50 -0300

    add middleware ensureAuthenticated

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,30 @@
+import { NextFunction, request, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+function ensureAuthenticated(req: Request, res: Response, next: NextFunction) {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const { sub } = verify(token, process.env.JWT_SECRET || "secret");
+
+    if (sub && typeof sub === "string") {
+      request.id = sub;
+    } else {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ message: "Expired token" });
+    }
+
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  next();
+}
+
+export default ensureAuthenticated;
