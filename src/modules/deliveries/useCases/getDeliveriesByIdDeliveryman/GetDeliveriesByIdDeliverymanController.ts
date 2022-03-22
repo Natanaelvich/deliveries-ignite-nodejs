ead33c0e10d6a6a5972331c348e9ab45fea54084
Natanael Lima <taelima1997@gmail.com>
2022-03-21 23:58:14 -0300

    add GetDeliveriesByIdDeliverymanController

---
+++ src/modules/deliveries/useCases/getDeliveriesByIdDeliveryman/GetDeliveriesByIdDeliverymanController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { GetDeliveriesByIdDeliverymanUseCase } from "./GetDeliveriesByIdDeliverymanUseCase";
+
+export class GetDeliveriesByIdDeliverymanController {
+  async handle(request: Request, response: Response) {
+      console.log('HEHEHEHEHEH')
+      
+    const { id } = request;
+
+
+    const getDeliveriesByIdDeliverymanUseCase =
+      new GetDeliveriesByIdDeliverymanUseCase();
+
+    const deliveries = await getDeliveriesByIdDeliverymanUseCase.execute({
+      idDeliveryman: id,
+    });
+
+    response.json(deliveries);
+  }
+}
