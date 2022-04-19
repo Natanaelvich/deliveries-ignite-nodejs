311ec75cf373e07ef1e23e41b7bc28defdb55823
Natanael Lima <taelima1997@gmail.com>
2022-04-18 23:50:49 -0300

    add modules dabatase and client and remove controllers and providers unused

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { DatabaseModule } from './database/database.module';
+import { ClientModule } from './client/client.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule, ClientModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
