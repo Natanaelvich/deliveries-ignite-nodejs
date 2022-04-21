c7152c451c58e63e1576c8cdeb570e9aefd76ed5
Natanael Lima <taelima1997@gmail.com>
2022-04-20 23:43:01 -0300

    add tests files authenticateclient module

---
+++ src/client/authenticateclient/authenticateclient.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthenticateclientController } from './authenticateclient.controller';
+import { AuthenticateclientService } from './authenticateclient.service';
+
+describe('AuthenticateclientController', () => {
+  let controller: AuthenticateclientController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AuthenticateclientController],
+      providers: [AuthenticateclientService],
+    }).compile();
+
+    controller = module.get<AuthenticateclientController>(AuthenticateclientController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
