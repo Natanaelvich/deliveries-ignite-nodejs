f035908ead83c808da1ad36feeb5f5802b92a725
Natanael Lima <taelima1997@gmail.com>
2022-04-20 23:42:33 -0300

    add AuthenticateclientModule import to clientmodule

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ClientService } from './client.service';
 import { ClientController } from './client.controller';
 import { PrismaService } from 'src/database/prisma/prisma.service';
+import { AuthenticateclientModule } from './authenticateclient/authenticateclient.module';
 
 @Module({
   controllers: [ClientController],
   providers: [ClientService, PrismaService],
+  imports: [AuthenticateclientModule],
 })
 export class ClientModule {}
