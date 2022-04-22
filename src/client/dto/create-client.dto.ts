a2d378703f8e955c9241b952f2436834a963648d
Natanaelvich <taelima1997@gmail.com>
2022-04-21 21:04:58 -0300

    add validation create client dto

--- src/client/dto/create-client.dto.ts
+++ src/client/dto/create-client.dto.ts
@@ -1,4 +1,10 @@
+import { IsNotEmpty, IsString } from 'class-validator';
 export class CreateClientDto {
+  @IsNotEmpty()
+  @IsString()
   username: string;
+
+  @IsNotEmpty()
+  @IsString()
   password: string;
 }
