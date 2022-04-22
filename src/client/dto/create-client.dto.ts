01e8ee269217019cf050d069cbb028e1f78a3317
Natanaelvich <taelima1997@gmail.com>
2022-04-21 21:28:40 -0300

    add ApiProperty to doc

--- src/client/dto/create-client.dto.ts
+++ src/client/dto/create-client.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 export class CreateClientDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   username: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   password: string;
 }
