dc8ebe69d5357198610609303b7dc088aa1193bb
Natanael Lima <taelima1997@gmail.com>
2022-04-24 15:08:41 -0300

    add resource delivery

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from './database/database.module';
 import { ClientModule } from './client/client.module';
+import { DeliveryModule } from './delivery/delivery.module';
 
 @Module({
-  imports: [DatabaseModule, ClientModule],
+  imports: [DatabaseModule, ClientModule, DeliveryModule],
   controllers: [],
   providers: [],
 })
