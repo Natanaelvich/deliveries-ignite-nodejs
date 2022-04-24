1e66cc80943698d2af03dc90ed24132056d08885
Natanael Lima <taelima1997@gmail.com>
2022-04-24 15:32:17 -0300

    update create dto

--- src/delivery/dto/create-delivery.dto.ts
+++ src/delivery/dto/create-delivery.dto.ts
@@ -1,1 +1,14 @@
-export class CreateDeliveryDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsEmpty, IsOptional } from 'class-validator';
+
+export class CreateDeliveryDto {
+  @IsString()
+  @IsOptional()
+  @ApiProperty()
+  id_deliveryman?: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
+  item_name: string;
+}
