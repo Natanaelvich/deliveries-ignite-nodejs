328e9aada22d577a0cbe6dc88c1bf01f16d0f749
Natanael Lima <taelima1997@gmail.com>
2022-04-28 00:00:50 -0300

    add providers

--- src/client/refreshtokenclient/refreshtokenclient.module.ts
+++ src/client/refreshtokenclient/refreshtokenclient.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 
+import { ClientService } from '../client.service';
+
 import { RefreshtokenclientService } from './refreshtokenclient.service';
 import { RefreshtokenclientController } from './refreshtokenclient.controller';
 
+import { PrismaService } from 'src/database/prisma/prisma.service';
+
 @Module({
   controllers: [RefreshtokenclientController],
-  providers: [RefreshtokenclientService],
+  providers: [RefreshtokenclientService, PrismaService, ClientService],
 })
 export class RefreshtokenclientModule {}
