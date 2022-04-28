088399ce8684efdf9e872141a5c4bf6e50c9348d
Natanael Lima <taelima1997@gmail.com>
2022-04-27 23:59:23 -0300

    update RefreshtokenclientController

--- src/client/refreshtokenclient/refreshtokenclient.controller.ts
+++ src/client/refreshtokenclient/refreshtokenclient.controller.ts
@@ -1,10 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+
+import { CreateRefreshtokenclient } from './dto/create-refreshtokenclient.dto';
 
 import { RefreshtokenclientService } from './refreshtokenclient.service';
 
+@ApiTags('refreshtokenclient')
 @Controller('refreshtokenclient')
 export class RefreshtokenclientController {
   constructor(
     private readonly refreshtokenclientService: RefreshtokenclientService,
   ) {}
+
+  @Post()
+  async create(@Body() { refresh_token }: CreateRefreshtokenclient) {
+    const refreshToken = await this.refreshtokenclientService.refresh({
+      refresh_token,
+    });
+
+    return refreshToken;
+  }
 }
