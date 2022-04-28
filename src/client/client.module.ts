1a0cc111d25a49a5b9ebb89666b59e1d7929ac24
Natanael Lima <taelima1997@gmail.com>
2022-04-27 23:38:53 -0300

    add imports Refreshtokenclient

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -5,11 +5,13 @@ import { ClientController } from './client.controller';
 
 import { AuthenticateclientModule } from './authenticateclient/authenticateclient.module';
 
+import { RefreshtokenclientModule } from './refreshtokenclient/refreshtokenclient.module';
+
 import { PrismaService } from 'src/database/prisma/prisma.service';
 
 @Module({
   controllers: [ClientController],
   providers: [ClientService, PrismaService],
-  imports: [AuthenticateclientModule],
+  imports: [AuthenticateclientModule, RefreshtokenclientModule],
 })
 export class ClientModule {}
