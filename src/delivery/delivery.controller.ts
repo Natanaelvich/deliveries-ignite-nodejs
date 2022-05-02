5211db1586a77f84b21e13ed37ab09fd4b4ace28
Natanael Lima <taelima1997@gmail.com>
2022-05-01 22:24:02 -0300

    remove try catch user and delivery controlers

--- src/delivery/delivery.controller.ts
+++ src/delivery/delivery.controller.ts
@@ -8,8 +8,6 @@ import {
   Delete,
   UseGuards,
   Request,
-  HttpException,
-  HttpStatus,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
@@ -31,23 +29,7 @@ export class DeliveryController {
     @Body()
     createDeliveryDto: CreateDeliveryDto,
   ) {
-    try {
-      const delivery = await this.deliveryService.create(
-        req.id,
-        createDeliveryDto,
-      );
-
-      return delivery;
-    } catch (error) {
-      if (error.message === 'CLIENT_NOT_FOUND') {
-        throw new HttpException(
-          'Your user is not a client',
-          HttpStatus.FORBIDDEN,
-        );
-      }
-
-      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+    return await this.deliveryService.create(req.id, createDeliveryDto);
   }
 
   @Get()
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,8 +6,6 @@ import {
   Patch,
   Param,
   Delete,
-  HttpStatus,
-  HttpException,
   UseGuards,
   Request,
 } from '@nestjs/common';
@@ -26,16 +24,7 @@ export class UserController {
 
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
-    try {
-      const user = await this.userService.create(createUserDto);
-      return user;
-    } catch (error) {
-      if (error.message === 'CLIENT_EXISTS') {
-        throw new HttpException('User already exists', HttpStatus.CONFLICT);
-      }
-
-      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+    return await this.userService.create(createUserDto);
   }
 
   @Get()
@@ -51,21 +40,7 @@ export class UserController {
   @Patch()
   @UseGuards(AuthorizationGuard)
   async update(@Request() req, @Body() updateUserDto: UpdateUserDto) {
-    try {
-      return await this.userService.update(req.id, updateUserDto);
-    } catch (error) {
-      if (error.message === 'CLIENT_EXISTS_WITH_USERNAME') {
-        throw new HttpException(
-          'User already exists with username',
-          HttpStatus.CONFLICT,
-        );
-      }
-      if (error.message === 'CLIENT_NOT_FOUND') {
-        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-      }
-
-      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+    return await this.userService.update(req.id, updateUserDto);
   }
 
   @Delete(':id')
